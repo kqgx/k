423cf9d7b8690faee53c4754fb39643bd86a0a9a
kqgx <2916070623@qq.com>
2021-12-20 17:18:01 +0800

    20211220

---
+++ data/log/error/log-2021-12-20.php
@@ -0,0 +1,60 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-12-20 10:50:47 --> Severity: error --> Exception: syntax error, unexpected '/' D:\phpstudy_pro\WWW\k\data\views\f00be82513d7b6e99e034d277bd9fa29 72
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 614
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\dd09d3b9826f69e07d479c197ca39fe2 101
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\dd09d3b9826f69e07d479c197ca39fe2 130
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 24
+ERROR - 2021-12-20 16:41:59 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 32
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 39
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 59
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 72
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 80
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 614
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 111
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 122
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 136
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 144
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 155
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 614
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 188
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 196
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 614
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 228
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1267
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 236
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\02299d0aa3445b79d5a5b4f740df7ba0 269
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant MOD_DIR - assumed 'MOD_DIR' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\data\views\7c3bcfd0641b743f94137421d8030182 15
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> Use of undefined constant DR_VERSION - assumed 'DR_VERSION' (this will throw an Error in a future version of PHP) D:\phpstudy_pro\WWW\k\data\views\7c3bcfd0641b743f94137421d8030182 48
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 16:42:00 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\7c3bcfd0641b743f94137421d8030182 53
+ERROR - 2021-12-20 17:00:45 --> 404 Page Not Found: admin//rule
+ERROR - 2021-12-20 17:02:07 --> 404 Page Not Found: admin//rule
+ERROR - 2021-12-20 17:02:27 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\web\libraries\Template\Template.php 1833
+ERROR - 2021-12-20 17:02:27 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable D:\phpstudy_pro\WWW\k\data\views\7c0a820df8d4b65283d2639ec979ebda 214
+ERROR - 2021-12-20 17:03:16 --> 404 Page Not Found: admin//rule
