714eb14ed0fce11cf7783ecab939ca18c8f9d620
kqgx <76797991+kqgx@users.noreply.github.com>
2022-07-05 11:15:29 +0800

    Delete server.php

--- server.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-
-if ($uri !== '/' && file_exists("{$_SERVER['DOCUMENT_ROOT']}$uri" . $uri)) {
-    return false;
-}
-require_once __DIR__ . '/public/index.php';
