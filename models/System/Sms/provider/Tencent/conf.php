d6948cf574aa045b0d820010be37c5f44bb155a5
kqgx <76797991+kqgx@users.noreply.github.com>
2022-07-05 11:11:38 +0800

    Delete models directory

--- models/System/Sms/provider/Tencent/conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'appid' => '1400172127',
-    'appkey' => 'a2bae5f331b0161541a870cc3501f294',
-    'template_id' => '249003',
-    'sign' => '典则读书'
-];
--- models/System/Sms/provider/provider.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-abstract class System_Sms_provider extends CI_Model
-{
-    abstract public function templateSend($phone, array $params, $template_id = null, $sign_name = null);
-}
